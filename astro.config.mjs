f7fa3b127ede6382f187929569a74183e711e221
Daniel Frates <dfrates@Daniels-MBP.hsd1.co.comcast.net>
2024-06-11 10:09:42 -0600

    Finish blog test site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dfratesblogtest.netlify.app/"
+  site: "https://dfratesblogtest.netlify.app/",
+  integrations: [preact()]
 });
